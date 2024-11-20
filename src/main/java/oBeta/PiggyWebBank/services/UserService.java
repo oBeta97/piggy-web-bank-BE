2fcd9955ca924ec32171b2ca4be3ec91962b3d6c
oBeta97 <paolopellizzari97@gmail.com>
2024-11-20 20:49:50 +0100

    changed gitignore

--- src/main/java/oBeta/PiggyWebBank/services/UserService.java
+++ src/main/java/oBeta/PiggyWebBank/services/UserService.java
@@ -1,4 +1,25 @@
 package oBeta.PiggyWebBank.services;
 
+import oBeta.PiggyWebBank.entities.User;
+import oBeta.PiggyWebBank.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UsersRepository usersRepo;
+
+    public Page<User> getAllUsers(int page, int size, String sortBy){
+        if(size > 50) size = 50;
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return this.featuresRepo.findAll(pageable);
+    }
+
 }
