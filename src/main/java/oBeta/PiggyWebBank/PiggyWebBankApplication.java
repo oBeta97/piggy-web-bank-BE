2bff5f13dedf990753b0e5dd52adbd792ce72f5b
oBeta97 <paolopellizzari97@gmail.com>
2024-11-20 14:28:37 +0100

    init project

---
+++ src/main/java/oBeta/PiggyWebBank/PiggyWebBankApplication.java
@@ -0,0 +1,13 @@
+package oBeta.PiggyWebBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggyWebBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiggyWebBankApplication.class, args);
+	}
+
+}
