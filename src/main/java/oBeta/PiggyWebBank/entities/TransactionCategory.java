9474a2378fabc29498207dc4c658e728303f1076
oBeta97 <paolopellizzari97@gmail.com>
2024-11-21 16:41:21 +0100

    added base TransactionCategory service

--- src/main/java/oBeta/PiggyWebBank/entities/TransactionCategory.java
+++ src/main/java/oBeta/PiggyWebBank/entities/TransactionCategory.java
@@ -6,6 +6,8 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import oBeta.PiggyWebBank.payloads.BaseTransactionCategoryDTO;
+import oBeta.PiggyWebBank.payloads.TransactionCategoryDTO;
 
 import java.util.UUID;
 
@@ -21,7 +23,7 @@ public class TransactionCategory {
     private long id;
 
     @Column(nullable = false, unique = true)
-    private long name;
+    private String name;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = true)
@@ -30,10 +32,22 @@ public class TransactionCategory {
     @Column(name = "is_expense", nullable = false)
     private Boolean isExpense;
 
-    public TransactionCategory(long name, User user, Boolean isExpense) {
+    public TransactionCategory(String name, User user, Boolean isExpense) {
         this.name = name;
         this.user = user;
         this.isExpense = isExpense;
+    }
+
+    public TransactionCategory(BaseTransactionCategoryDTO dto){
+        this.name = dto.name();
+        this.user = null;
+        this.isExpense = dto.isExpense();
+    }
+
+    public TransactionCategory(TransactionCategoryDTO dto, User user){
+        this.name = dto.name();
+        this.user = user;
+        this.isExpense = dto.isExpense();
     }
 }
 
--- src/main/java/oBeta/PiggyWebBank/repositories/TransactionCategoriesRepository.java
+++ src/main/java/oBeta/PiggyWebBank/repositories/TransactionCategoriesRepository.java
@@ -1,9 +1,21 @@
 package oBeta.PiggyWebBank.repositories;
 
 import oBeta.PiggyWebBank.entities.TransactionCategory;
+import oBeta.PiggyWebBank.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TransactionCategoriesRepository extends JpaRepository<TransactionCategory, Long> {
+
+    @Query("SELECT tc FROM TransactionCategory tc WHERE tc.user IS NULL")
+    List<TransactionCategory> findBaseTransactionCategory();
+
+    List<TransactionCategory> findTransactionCategoryByUser(User user);
+
+    @Query("SELECT tc FROM TransactionCategory tc WHERE tc.user IS NULL OR tc.user = :user  ")
+    List<TransactionCategory> findAllTransactionCategoryOfAnUser(User user);
 }
