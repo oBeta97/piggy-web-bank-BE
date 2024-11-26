152e951f7e8a959815b4419bf81313aaa373e4a1
oBeta97 <paolopellizzari97@gmail.com>
2024-11-26 22:02:35 +0100

    Added base me controller endpoint

--- src/main/java/oBeta/PiggyWebBank/controllers/me/MeController.java
+++ src/main/java/oBeta/PiggyWebBank/controllers/me/MeController.java
@@ -1,16 +1,34 @@
 package oBeta.PiggyWebBank.controllers.me;
 
 
+import oBeta.PiggyWebBank.entities.Role;
 import oBeta.PiggyWebBank.entities.User;
+import oBeta.PiggyWebBank.entities.UserCharacteristic;
+import oBeta.PiggyWebBank.services.UserCharacteristicsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/me")
 public class MeController {
+
+    @Autowired
+    private UserCharacteristicsService userCharacteristicsService;
 
     @GetMapping
     public User getMe(@AuthenticationPrincipal User loggedUser){
         return loggedUser;
+    }
+
+    @GetMapping("/roles")
+    public Role getMeRole(@AuthenticationPrincipal User loggedUser){
+        return loggedUser.getRole();
+    }
+
+    @GetMapping("/user-characteristics")
+    public UserCharacteristic getMeUserCharacteristics(@AuthenticationPrincipal User loggedUser){
+        return this.userCharacteristicsService.getUserCharacteristicByUser(loggedUser);
     }
 
 }
